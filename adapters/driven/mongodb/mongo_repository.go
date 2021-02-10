9af1694854c5684b3acb4cb9ee700b65c1f7fb69
psghahremni <ps.ghahremni@gmail.com>
2021-02-10 13:26:25 +0330

    Add Automatic expiration for stored urls

--- adapters/driven/mongodb/mongo_repository.go
+++ adapters/driven/mongodb/mongo_repository.go
@@ -19,11 +19,7 @@ type mongoRepository struct {
 	databaseName string
 }
 
-func newMongoClient(mongoConnectionUrl string, timeout time.Duration) (*mongo.Client, error) {
-	// Make a context based on the given timeout.
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
+func newMongoClient(ctx context.Context, mongoConnectionUrl string) (*mongo.Client, error) {
 	// Create a client using the given URL.
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoConnectionUrl))
 	if err != nil {
@@ -39,8 +35,26 @@ func newMongoClient(mongoConnectionUrl string, timeout time.Duration) (*mongo.Cl
 }
 
 func NewMongoRepository(mongoConnectionUrl, databaseName string, timeout time.Duration) (ports.UrlRepository, error) {
+	// Make a context based on the given timeout.
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
 	// Create a MongoDB client using the given URL.
-	client, err := newMongoClient(mongoConnectionUrl, timeout)
+	client, err := newMongoClient(ctx, mongoConnectionUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create an index on "expires_at" key, and automatically expire documents with this key.
+	var expireAfterSeconds int32 = 0
+	_, err = client.Database(databaseName).Collection("urls").Indexes().CreateOne(ctx, mongo.IndexModel{
+		Keys: bson.M{
+			"expires_at": 1,
+		},
+		Options: &options.IndexOptions{
+			ExpireAfterSeconds: &expireAfterSeconds,
+		},
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -55,7 +69,6 @@ func NewMongoRepository(mongoConnectionUrl, databaseName string, timeout time.Du
 
 /* The following methods are the implementation of UrlRepository specified in domain ports. */
 
-// TODO: Automatically remove expired documents.
 func (mongoRepository *mongoRepository) StoreUrl(ctx context.Context, shortenedUrl domainModels.ShortenedUrl) error {
 	// Convert domain model to repository model.
 	shortenedUrlDto := models.ShortenedUrl{
